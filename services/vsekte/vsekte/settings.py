fec27d65a52deb9fcec307b534abf6ae9b2e9c38
meSerge8 <melnikov.serge8@gmail.com>
2022-04-17 15:49:40 +0700

    docker + python + postgres

--- services/vsekte/vsekte/settings.py
+++ services/vsekte/vsekte/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'vsekte.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'secta',
+        'USER': 'postgres',
+        'PASSWORD': 'avesatan666',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
