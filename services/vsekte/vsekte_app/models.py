c7969e4a4e84ded811864d3fb98b14523336dfd0
Natalya Shkolnaya <nkashk77@gmail.com>
2022-04-17 16:39:04 +0700

    add models

--- services/vsekte/vsekte_app/models.py
+++ services/vsekte/vsekte_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Sektant(models.Model):
+    username = models.fields.CharField(max_length=50)
+    password = models.fields.CharField(max_length=32)
+
+
+class Sekta(models.Model):
+    sektaname = models.fields.CharField(max_length=50)
+    private_key = models.fields.CharField(max_length=32)
+    participants = models.ManyToManyField(Sektant,blank=True)
+
+
+class Nickname(models.Model):
+    sektant = models.OneToOneField(Sektant)
+    sekta = models.OneToOneField(Sekta)
+    nickname = models.fields.CharField(max_length=50)
+
+
